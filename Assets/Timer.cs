3d985fa6c602cafd22271b17ca9b3a0a9733a8f1
valeriequintana <qvalerie@live.com>
2016-05-05 22:08:18 -0400

    Hover Over
    Version 1.0
    5.5.16

---
+++ Assets/Timer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    public Text timerText;
+    private float startTime;
+    private bool finnished = false;
+
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (finnished)
+            return;
+        float t = Time.time - startTime;
+
+        string minutes = ((int) t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        timerText.text = minutes + ":" + seconds;
+	
+	}
+    
+    public void Finnish()
+    {
+        finnished = true;
+        timerText.color = Color.yellow;
+    }
+}
---
+++ Assets/WinBox.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinBox : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject.Find("Car").SendMessage("Finnish");
+    }
+}
